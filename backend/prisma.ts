aca3df5335cca76136c7e77a5ec549329894f6c9
jaketux <169253660+jaketux@users.noreply.github.com>
2025-07-28 07:12:11 +1000

    Added deletemany to gamesession whilst building site to stop foreign key errors.

--- backend/prisma.ts
+++ backend/prisma.ts
@@ -3,6 +3,7 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function main() {
+  await prisma.gameSession.deleteMany();
   await prisma.character.deleteMany();
   await prisma.map.deleteMany();
 
